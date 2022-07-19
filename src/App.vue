6478a7124e98f019bc8d15cd572a9f5b77a1c640
Sam Barnard <sam@DESKTOP-KK2KJEE.localdomain>
2022-07-19 18:14:04 +0100

    Creating a nicer interface that holds the calculator along with a box illustrating modifiable props.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <script setup lang="ts">
+import { ref } from '@vue/reactivity'
 import Calculator from './components/Calculator.vue'
+
+const decimalPointCap = ref(4)
 </script>
 
 <template>
   <div class="container mx-auto px-2 text-center">
-    <div class="py-12">
-      <h1 class="text-6xl font-bold mb-4">The Calculator</h1>
-      <p>A basic number calculator built using Vue3 + TypeScript & TailwindCSS.</p>
+    <div class="py-12 space-y-12">
+      <div>
+        <h1 class="text-6xl font-bold mb-4">The Calculator</h1>
+        <p>A basic number calculator built using Vue3 + TypeScript & TailwindCSS.</p>
+      </div>
+      <div class="text-left p-4 max-w-md mx-auto border border-white rounded-lg">
+        <p class="font-bold">Props:</p>
+        <div class="flex items-center space-x-2">
+          <p><code>decimalPointCap</code></p>
+          <input
+            type="number"
+            v-model="decimalPointCap"
+            min="0"
+            max="6"
+            class="text-black text-center"
+          />
+        </div>
+      </div>
     </div>
     <Calculator
-      :decimal-point-cap="4"
-      class="max-w-md mx-auto"
+      :key="`using-decimal-point--${decimalPointCap}`"
+      :decimal-point-cap="decimalPointCap"
+      class="max-w-md mx-auto rounded-lg overflow-hidden bg-slate-600 border-2 border-white drop-shadow-md"
     />
   </div>
 </template>
